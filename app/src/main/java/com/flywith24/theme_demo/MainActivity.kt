bb6ac208cbb62c659a2983b389e6d12f45e62d02
Flywith24 <fly_with24@163.com>
2020-06-23 19:59:26 +0800

    根据选中颜色改变 primary color

--- app/src/main/java/com/flywith24/theme_demo/MainActivity.kt
+++ app/src/main/java/com/flywith24/theme_demo/MainActivity.kt
@@ -1,21 +1,35 @@
 package com.flywith24.theme_demo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
+import androidx.activity.viewModels
+import androidx.annotation.StyleableRes
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.content.ContextCompat
+import androidx.appcompat.widget.AppCompatRadioButton
+import androidx.core.widget.CompoundButtonCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val a = ContextCompat.getColor(this, R.color.purple_500)
-        val b = ContextCompat.getColor(btnTheme.context, R.color.purple_500)
-        Log.d(TAG, "onCreate: $a")
-        Log.d(TAG, "onCreate: $b")
+    private val mViewModel by viewModels<ThemeViewModel>()
 
-        radioGroup.setOnCheckedChangeListener { _, checkedId ->
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        mViewModel.primaryColor.value?.let {
+            setTheme(it)
+        }
+
+        super.onCreate(savedInstanceState)
+
+        initRadioButton()
+
+        rgPrimary.setOnCheckedChangeListener { group, checkedId ->
+            mViewModel.primaryColor.value =
+                group.findViewById<AppCompatRadioButton>(checkedId).tag as Int
+            recreate()
+        }
+        rgTheme.setOnCheckedChangeListener { _, checkedId ->
             when (checkedId) {
                 R.id.themeLight ->
                     delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_NO
@@ -23,7 +37,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
                 R.id.themeNight ->
                     delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_YES
 
-                R.id.themeDefault->
+                R.id.themeDefault ->
                     delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
             }
         }
@@ -31,7 +45,35 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     }
 
+    private fun initRadioButton() {
+        val primaryArray = resources.obtainTypedArray(R.array.primary_palettes)
+        val descriptionArray = resources.obtainTypedArray(R.array.palettes_content_description)
+        var style: Int
+        if (primaryArray.length() != descriptionArray.length()) {
+            throw IllegalArgumentException("primary_palettes 与 palettes_content_description 长度不等！")
+        }
+        for (index in 0 until primaryArray.length()) {
+            style = primaryArray.getResourceId(index, 0)
+            rgPrimary.addView(AppCompatRadioButton(this).apply {
+                text = descriptionArray.getString(index)
+                val a = obtainStyledAttributes(style, PRIMARY_THEME_OVERLAY_ATTRS)
+                CompoundButtonCompat.setButtonTintList(
+                    this, ColorStateList.valueOf(a.getColor(0, Color.TRANSPARENT))
+                )
+                tag = style
+                a.recycle()
+            })
+        }
+        primaryArray.recycle()
+        descriptionArray.recycle()
+    }
+
     companion object {
         private const val TAG = "yyz11"
+
+        @StyleableRes
+        private val PRIMARY_THEME_OVERLAY_ATTRS = intArrayOf(
+            R.attr.colorPrimary, R.attr.colorPrimaryDark
+        )
     }
 }
---
+++ app/src/main/java/com/flywith24/theme_demo/ThemeViewModel.kt
@@ -0,0 +1,15 @@
+package com.flywith24.theme_demo
+
+import androidx.annotation.StyleRes
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+/**
+ * @author Flywith24
+ * @date   2020/6/23
+ * time   19:13
+ * description
+ */
+class ThemeViewModel : ViewModel() {
+    val primaryColor = MutableLiveData<@StyleRes Int>()
+}
